19ce6a148b1d4563ac6a5c2bf22fd8e0e2c34907
2Nonku123 <67873513+2Nonku123@users.noreply.github.com>
2022-03-11 18:22:50 +0200

    fruit

---
+++ test/appTest.js
@@ -0,0 +1,12 @@
+const assert = chai.assert;
+
+  describe('getRandom()', function(){
+    it('Should be able to set and get user Input', function(){
+      //let result = app.sayHello();
+      assert.equal('hello', 'hello');
+    });
+  });
+
+
+
+mocha.run();
---
+++ test/js/fruits.js
@@ -0,0 +1,59 @@
+//SetData To LocalStorage
+if(localStorage.getItem('fruitNames') === null) {
+  localStorage.setItem('fruitNames', '["Grapes", "Melon", "Watermelon", "Tangerine", "Lemon", "Banana", "Pineapple", "Mango", "Red Apple"]');
+}
+
+if(localStorage.getItem('fruitEmojis') === null) {
+  localStorage.setItem('fruitEmojis', '["🍇", "🍈", "🍉", "🍊", "🍋", "🍌", "🍍", "🥭", "🍎"]');
+}
+
+//View the localstorage data
+// Check if Fruit Name Data is Available
+if(localStorage.getItem('fruitNames') !=null) {
+  const fruitNam = JSON.parse(localStorage.getItem('fruitNames'));
+  const fruitEmo = JSON.parse(localStorage.getItem('fruitEmojis'));
+   //map the two arrays to a single array
+   const fruitArray = fruitNam.map(function(e, i){
+     return '<li class="list-group-item">'+ [e, fruitEmo[i]] + '</li>';
+   });
+//sort results in alphabetical order
+fruitArray.sort();
+//Display Results On Web Page 
+document.getElementById('fruit-joined').innerHTML = fruitArray.join('');
+console.log(fruitArray);
+}
+
+
+//adding a new fruit
+function addFruit() {
+
+  //get form data from the form
+  const getFruitName = document.querySelector( "#fruitName").value;
+  const getFruitEmoji = document.querySelector( "#fruitEmoji").value;
+
+  const oldFruitName =  JSON.parse(localStorage.getItem('fruitNames'));
+  oldFruitName.push(getFruitName);
+
+  // Save the old and new data to local storage
+  localStorage.setItem('fruitNames', JSON.stringify(oldFruitName));
+  const oldFruitEmoji =  JSON.parse(localStorage.getItem('fruitEmojis'));
+  oldFruitEmoji.push(getFruitEmoji);
+  // // Save the old and new data to local storage
+  localStorage.setItem('fruitEmojis', JSON.stringify(oldFruitEmoji));
+
+}
+
+function searchFruit() {
+  const fruitNam = JSON.parse(localStorage.getItem('fruitNames'));
+  //get Search Term
+  const getSearchTerm = document.querySelector( "#searchTerm").value;
+
+  console.log(fruitNam);
+  console.log(getSearchTerm);
+
+  const searchResult = fruitNam.find(e => getSearchTerm % 2 == 0);
+
+  console.log(searchResult);
+
+
+}
